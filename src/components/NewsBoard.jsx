55812a43a1def27d74ee16583c72e4e5730afb07
saim1123 <saimtahir3211@gmail.com>
2024-01-10 16:00:12 +0500

    push the project on github.

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -4,18 +4,17 @@ import NewsItem from "./NewsItem";
 
 let key = "78ddb99fa1cf415b870b89a4d047adbe";
 
-const NewsBoard = () => {
+const NewsBoard = ({ category }) => {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${key}`;
-    console.log(url);
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${key}`;
 
     fetch(url)
       .then((res) => res.json())
       .then((data) => setArticles(data.articles))
       .catch((err) => console.error(err));
-  });
+  }, [category]);
 
   return (
     <div>
